ad69d69090013c0f894ae18cacba652626f04ba7
iberdinsky-skilld <iberdinsky@skilld.cloud>
2026-02-04 18:55:06 +0100

    Init stoprybook

---
+++ .storybook/main.js
@@ -0,0 +1,18 @@
+
+
+/** @type { import('@storybook/html-vite').StorybookConfig } */
+const config = {
+  stories: ['../components/**/*.component.yml'],
+  addons: [
+    {
+      name: 'storybook-addon-sdc',
+      options: {
+        sdcStorybookOptions: {
+          twigLib: 'twing',
+        },
+      },
+    },
+  ],
+  "framework": "@storybook/html-vite"
+};
+export default config;
